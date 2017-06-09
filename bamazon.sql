31ad9fbea804a50e42ebdefd0304d0a929b4aa84
Joseph Lin <Joseph Lin>
2017-06-08 17:21:47 -0700

    add

--- bamazon.sql
+++ bamazon.sql
@@ -13,6 +13,21 @@ primary key(id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quanity)
-VALUES ("fidget spinner", "toys", 15 , 10);
-
+VALUES ("red fidget spinner", "toys", 7 , 10);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("blue fidget spinner", "toys", 15 , 8);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("purple fidget spinner", "toys", 18 , 6);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("black fidget spinner", "toys", 15 , 5);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("white fidget spinner", "toys", 24 , 9);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("green fidget spinner", "toys", 23 , 8);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("yellow fidget spinner", "toys", 19 , 8);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("orange fidget spinner", "toys", 13 , 3);
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("brown fidget spinner", "toys", 200 , 1);
 SELECT * FROM products; 
